a59d490148a29032a48ff38b6162bf10c94b9196
richhapal <richhapal.16jccs098@jietjodhpur.ac.in>
2024-11-30 23:08:20 +0530

    feat: add category list

---
+++ src/app/category-list/page.tsx
@@ -0,0 +1,11 @@
+import CategoryList from "@/_components/categoryList/categoryList";
+
+const Page: React.FC = () => {
+  return (
+    <div>
+      <CategoryList />
+    </div>
+  );
+};
+
+export default Page;
