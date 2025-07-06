d8409f6d2f416074d457de084d9a1ec2d92fd2a7
richhapal <richhapal.16jccs098@jietjodhpur.ac.in>
2025-07-06 19:12:19 +0530

    update script

---
+++ src/app/productList/edit/layout.tsx
@@ -0,0 +1,18 @@
+import Script from "next/script";
+import { ReactNode } from "react";
+
+export default function EditProductLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <>
+      <Script
+        src="https://assets.geeksforgeeks.org/tinymce/5.10.9/tinymce.min.js"
+        strategy="beforeInteractive"
+      />
+      <>{children}</>
+    </>
+  );
+}
